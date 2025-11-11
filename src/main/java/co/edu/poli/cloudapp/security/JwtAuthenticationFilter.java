4cf0c1a55b5fc3b12a71be581a13fdc90840eade
Jose Rodriguez <joseluis.rodriguezlagos@bsci.com>
2025-11-11 11:32:32 -0500

    Filtro de JWT

---
+++ src/main/java/co/edu/poli/cloudapp/security/JwtAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package co.edu.poli.cloudapp.security;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil){
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            try {
+                var claims = jwtUtil.validateTokenAndGetClaims(token);
+                String sub = claims.getSubject();
+                // For simplicity, grant a default role
+                var auth = new UsernamePasswordAuthenticationToken(sub, null, List.of(new SimpleGrantedAuthority("ROLE_USER")));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (Exception ex) {
+                // invalid token -> clear context
+                SecurityContextHolder.clearContext();
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
